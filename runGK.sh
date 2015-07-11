b691f7a01a3fd8342a0916d8c8de365422b561a8
Michał Budzyński <michal@virtualdesign.pl>
2015-07-11 01:01:00 +0100

    :bento: VMstatus fixed

--- runGK.sh
+++ runGK.sh
@@ -3,8 +3,12 @@
 echo "Waiting for VM..."
 vmstatus=$(VBoxManage list vms -l | grep -e ^Name: -e ^State | sed s/\ \ //g | awk '/GK-DEV/{getline; print}')
 vmstatus=${vmstatus#*: }
+vmstatus=$(echo $vmstatus | sed -e 's/([^()]*)//g')
 
-echo $vmstatus
+if [[ $vmstatus == "powered off " ]]; then
+  echo "off!"
+fi
+
 # sleep 30
 # echo "IP:" `VBoxManage guestproperty get GK-DEV "/VirtualBox/GuestInfo/Net/0/V4/IP" | awk '{ print $2 }'`
 # echo "PASSWORD: developer"
