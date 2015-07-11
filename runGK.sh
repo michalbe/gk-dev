ce866e0286427b4d1919ef4c515f7313ee93f373
Michał Budzyński <michal@virtualdesign.pl>
2015-07-11 01:01:00 +0100

    :kissing: rename RO variable status to vmstatus

--- runGK.sh
+++ runGK.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # VBoxManage startvm GK-DEV
 echo "Waiting for VM..."
-status=$(VBoxManage list vms -l | grep -e ^Name: -e ^State | sed s/\ \ //g | awk '/GK-DEV/{getline; print}')
+vmstatus=$(VBoxManage list vms -l | grep -e ^Name: -e ^State | sed s/\ \ //g | awk '/GK-DEV/{getline; print}')
 
-echo $status
+echo $vmstatus
 # sleep 30
 # echo "IP:" `VBoxManage guestproperty get GK-DEV "/VirtualBox/GuestInfo/Net/0/V4/IP" | awk '{ print $2 }'`
 # echo "PASSWORD: developer"
