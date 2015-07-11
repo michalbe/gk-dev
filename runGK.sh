9a529eea7d9cde6ff24a5c829e2fdea75079c0d5
Michał Budzyński <michal@virtualdesign.pl>
2015-07-11 01:01:00 +0100

    :nose: display vm status in cl

--- runGK.sh
+++ runGK.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-VBoxManage startvm GK-DEV
+# VBoxManage startvm GK-DEV
 echo "Waiting for VM..."
-sleep 30
-echo "IP:" `VBoxManage guestproperty get GK-DEV "/VirtualBox/GuestInfo/Net/0/V4/IP" | awk '{ print $2 }'`
-echo "PASSWORD: developer"
-ssh developer@`VBoxManage guestproperty get GK-DEV "/VirtualBox/GuestInfo/Net/0/V4/IP" | awk '{ print $2 }'`
+status=$(VBoxManage list vms -l | grep -e ^Name: -e ^State | sed s/\ \ //g | awk '/GK-DEV/{getline; print}')
+
+echo $status
+# sleep 30
+# echo "IP:" `VBoxManage guestproperty get GK-DEV "/VirtualBox/GuestInfo/Net/0/V4/IP" | awk '{ print $2 }'`
+# echo "PASSWORD: developer"
+# ssh developer@`VBoxManage guestproperty get GK-DEV "/VirtualBox/GuestInfo/Net/0/V4/IP" | awk '{ print $2 }'`
