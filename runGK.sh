3cce0aa3a2d3bc02b4c23c83b198b54b7ffed22e
Michał Budzyński <michal@virtualdesign.pl>
2015-07-13 01:01:00 +0100

    :trollface: determine if vm standup

--- runGK.sh
+++ runGK.sh
@@ -1,17 +1,30 @@
 #!/bin/bash
-# VBoxManage startvm GK-DEV
+#VBoxManage startvm GK-DEV
 echo "Waiting for VM..."
 vmstatus=$(VBoxManage list vms -l | grep -e ^Name: -e ^State | sed s/\ \ //g | awk '/GK-DEV/{getline; print}')
 vmstatus=${vmstatus#*: }
 vmstatus=$(echo $vmstatus | sed -e 's/([^()]*)//g' | sed -e 's/[[:space:]]*$//')
 
-if [[ $vmstatus == "powered off" ]]; then
-  echo "off!"
-else
-  echo $vmstatus
-fi
+ip_ready(){
+  ip=`VBoxManage guestproperty get GK-DEV "/VirtualBox/GuestInfo/Net/0/V4/IP"`
 
-# sleep 30
-# echo "IP:" `VBoxManage guestproperty get GK-DEV "/VirtualBox/GuestInfo/Net/0/V4/IP" | awk '{ print $2 }'`
-# echo "PASSWORD: developer"
-# ssh developer@`VBoxManage guestproperty get GK-DEV "/VirtualBox/GuestInfo/Net/0/V4/IP" | awk '{ print $2 }'`
+  if [[ $ip == "No value set!" ]]; then
+    return 1;
+  else
+    return 0;
+  fi
+  # echo "PASSWORD: developer"
+  # ssh developer@`VBoxManage guestproperty get GK-DEV "/VirtualBox/GuestInfo/Net/0/V4/IP" | awk '{ print $2 }'`
+}
+
+if [[ $vmstatus == "powered off" ]]; then
+  echo "VM is not running. Pls start VM first."
+  exit 1;
+else
+  while ( ! ip_ready ); do
+    sleep 3
+    echo 'Waiting for machine to stand up...'
+  done
+
+  echo 'wstala!'
+fi
